90c914793ca972a0197979c550e03ddf279f4504
codezwjava <1563330148@qq.com>
2020-12-02 19:18:16 +0800

    。。。试一下

--- spring-zwstart/src/main/java/com/zwstart/service/XmlApplicationConfigText.java
+++ spring-zwstart/src/main/java/com/zwstart/service/XmlApplicationConfigText.java
@@ -3,6 +3,9 @@ package com.zwstart.service;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/**
+ * 自己看源码的入口
+ */
 public class XmlApplicationConfigText {
 	public static void main(String[] args) {
 		ApplicationContext applicationContext =

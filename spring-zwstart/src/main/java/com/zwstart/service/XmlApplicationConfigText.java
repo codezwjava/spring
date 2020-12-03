34680a60e27a0fffa73e4f82f24c13a4330c7185
codezwjava <1563330148@qq.com>
2020-12-03 21:14:56 +0800

    回退的版本

--- spring-zwstart/src/main/java/com/zwstart/service/XmlApplicationConfigText.java
+++ spring-zwstart/src/main/java/com/zwstart/service/XmlApplicationConfigText.java
@@ -3,9 +3,6 @@ package com.zwstart.service;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * 自己看源码的入口
- */
 public class XmlApplicationConfigText {
 	public static void main(String[] args) {
 		ApplicationContext applicationContext =
